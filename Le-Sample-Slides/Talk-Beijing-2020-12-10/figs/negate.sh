29be8df71b29cbe7fda3e90155d126223c1d5ec7
chenle02 <chenle02@gmail.com>
2020-12-07 23:24:46 -0500

    Add source files for my talk to be given in Dec. 10th, the same day.

---
+++ Le-Sample-Slides/Talk-Beijing-2020-12-10/figs/negate.sh
@@ -0,0 +1,131 @@
+
+Emory-neg.png
+Emory.png
+picture-tug-war2.jpg
+picture-tug-war.jpg
+SHE_Mindmap2.aux
+SHE_Mindmap2.log
+SHE_Mindmap2.pdf
+SHE_Mindmap2.synctex.gz
+SHE_Mindmap2.tex
+SHE_Mindmap.aux
+SHE_Mindmap.dvi
+SHE_Mindmap.eps
+SHE_Mindmap.log
+SHE_Mindmap.pdf
+SHE_Mindmap.synctex.gz
+SHE_Mindmap.tex
+Sin_L0.eps
+Sin_L0.jpeg
+Sin_L0.pdf
+Sin_L1.eps
+Sin_L1.jpeg
+Sin_L1.pdf
+Sin_L2.eps
+Sin_L2.jpeg
+Sin_L2.pdf
+Sin_L3.eps
+Sin_L3.jpeg
+Sin_L3.pdf
+Sin_L4.eps
+Sin_L4.jpeg
+Sin_L4.pdf
+
+convert Sin_L0.jpeg -negate Sin_L0-neg.jpeg
+convert Sin_L1.jpeg -negate Sin_L1-neg.jpeg
+convert Sin_L2.jpeg -negate Sin_L2-neg.jpeg
+convert Sin_L3.jpeg -negate Sin_L3-neg.jpeg
+
+convert small_Delta_L0_cut.jpeg -negate small_Delta_L0_cut-neg.jpeg
+convert small_Delta_L2_cut.jpeg -negate small_Delta_L2_cut-neg.jpeg
+convert small_Delta_L4_cut.jpeg -negate small_Delta_L4_cut-neg.jpeg
+convert small_Delta_L5_cut.jpeg -negate small_Delta_L5_cut-neg.jpeg
+convert small_Delta_L6_cut.jpeg -negate small_Delta_L6_cut-neg.jpeg
+convert small_Delta_L8_cut.jpeg -negate small_Delta_L8_cut-neg.jpeg
+
+convert small_Delta_L0.jpeg -negate small_Delta_L0-neg.jpeg
+convert small_Delta_L2.jpeg -negate small_Delta_L2-neg.jpeg
+convert small_Delta_L4.jpeg -negate small_Delta_L4-neg.jpeg
+convert small_Delta_L5.jpeg -negate small_Delta_L5-neg.jpeg
+convert small_Delta_L6.jpeg -negate small_Delta_L6-neg.jpeg
+convert small_Delta_L8.jpeg -negate small_Delta_L8-neg.jpeg
+
+
+convert picture-tug-war2.jpg -negate picture-tug-war2-neg.jpg
+
+convert small_Delta_L0_cut.jpeg
+convert small_Delta_L0_cut.pdf
+convert small_Delta_L0.dat
+convert small_Delta_L0.eps
+convert small_Delta_L0.jpeg
+convert small_Delta_L0.pdf
+convert small_Delta_L10_cut.eps
+convert small_Delta_L10_cut.jpeg
+convert small_Delta_L10_cut.pdf
+convert small_Delta_L10.dat
+convert small_Delta_L10.eps
+convert small_Delta_L10.jpeg
+convert small_Delta_L10.pdf
+convert small_Delta_L1_cut.eps
+convert small_Delta_L1_cut.jpeg
+convert small_Delta_L1_cut.pdf
+convert small_Delta_L1.dat
+convert small_Delta_L1.eps
+convert small_Delta_L1.jpeg
+convert small_Delta_L1.pdf
+convert small_Delta_L2_cut.eps
+convert small_Delta_L2_cut.jpeg
+convert small_Delta_L2_cut.pdf
+convert small_Delta_L2.dat
+convert small_Delta_L2.eps
+convert small_Delta_L2.jpeg
+convert small_Delta_L2.pdf
+convert small_Delta_L3_cut.eps
+convert small_Delta_L3_cut.jpeg
+convert small_Delta_L3_cut.pdf
+convert small_Delta_L3.dat
+convert small_Delta_L3.eps
+convert small_Delta_L3.jpeg
+convert small_Delta_L3.pdf
+convert small_Delta_L4_cut.eps
+convert small_Delta_L4_cut.jpeg
+convert small_Delta_L4_cut.pdf
+convert small_Delta_L4.dat
+convert small_Delta_L4.eps
+convert small_Delta_L4.jpeg
+convert small_Delta_L4.pdf
+convert small_Delta_L5_cut.eps
+convert small_Delta_L5_cut.jpeg
+convert small_Delta_L5_cut.pdf
+convert small_Delta_L5.dat
+convert small_Delta_L5.eps
+convert small_Delta_L5.jpeg
+convert small_Delta_L5.pdf
+convert small_Delta_L6_cut.eps
+convert small_Delta_L6_cut.jpeg
+convert small_Delta_L6_cut.pdf
+convert small_Delta_L6.dat
+convert small_Delta_L6.eps
+convert small_Delta_L6.jpeg
+convert small_Delta_L6.pdf
+convert small_Delta_L7_cut.eps
+convert small_Delta_L7_cut.jpeg
+convert small_Delta_L7_cut.pdf
+convert small_Delta_L7.dat
+convert small_Delta_L7.eps
+convert small_Delta_L7.jpeg
+convert small_Delta_L7.pdf
+convert small_Delta_L8_cut.eps
+convert small_Delta_L8_cut.jpeg
+convert small_Delta_L8_cut.pdf
+convert small_Delta_L8.dat
+convert small_Delta_L8.eps
+convert small_Delta_L8.jpeg
+convert small_Delta_L8.pdf
+convert small_Delta_L9_cut.eps
+convert small_Delta_L9_cut.jpeg
+convert small_Delta_L9_cut.pdf
+convert small_Delta_L9.dat
+convert small_Delta_L9.eps
+convert small_Delta_L9.jpeg
+convert small_Delta_L9.pdf
