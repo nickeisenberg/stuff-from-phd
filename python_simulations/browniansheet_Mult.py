4ee0d99380b57971ad0fe54bcd1aea14b328cc88
chenle02 <chenle02@gmail.com>
2022-03-02 11:05:37 -0600

    Update the code for BSheet simulation.

--- python_simulations/browniansheet_Mult.py
+++ python_simulations/browniansheet_Mult.py
@@ -1,8 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    python_simulations.browniansheet_Mult
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    This is a simulation for Brownian Sheet....
+
+    :copyright: (c) 2022 by Nick Eisenberg <nickeisenberg@gmail.com>.
+    :license: LICENSE_NAME, see LICENSE for more details.
+    :created at Wed 02 Mar 2022 10:55:11 AM CST
+"""
+
 # A simulation of a Brownian sheet though multiplication of independent Brownian motions
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+# import math
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 # Set the time and space intervals. Choose number of partitions of each.
@@ -25,19 +38,19 @@ x_axis = np.linspace(0.0, x, nx + 1)
 W_tx = np.zeros((nx + 1, nt + 1))
 for x in range(nx):
     for t in range(nt):
-        dW = np.random.normal(0, delta_t * delta_x, size=nt )
-        W_tx[x + 1, t + 1] = W_tx[x, t + 1] + W_tx[x + 1, t] - W_tx[x, t] + dW[t]
-        
+        dW = np.random.normal(0, delta_t * delta_x, size=1)
+        W_tx[x + 1, t + 1] = W_tx[x, t + 1] + W_tx[x + 1, t] - W_tx[x, t] + dW[0]
+
 # print(W_tx)
 
 # Plot the Brownian Sheet
 ts, xs = np.meshgrid(t_axis, x_axis)
-fig = plt.figure(figsize=(8,8))
+fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot(111, projection='3d')
-#ax = Axes3D(fig, auto_add_to_figure=False)
+# ax = Axes3D(fig, auto_add_to_figure=False)
 fig.add_axes(ax)
 # ax.set_xlabel('$ 0 \\leq t \\leq {} $'.format(t), fontsize=14)
 # ax.set_ylabel('$0 \\leq x \\leq \\pi$', fontsize=14)
 ax.plot_surface(ts, xs, W_tx, rstride=1, cstride=1, cmap='plasma')
-ax.set_title('Brownian Sheet',fontsize=20)
+ax.set_title('Brownian Sheet', fontsize=20)
 plt.show()
