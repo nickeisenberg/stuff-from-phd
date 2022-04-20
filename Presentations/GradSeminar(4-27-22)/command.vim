d5bc3ad24bf9f714657c9061644b36c167b181c6
chenle02 <chenle02@gmail.com>
2022-04-20 12:57:16 -0500

    Demo for python setup for Nick.

---
+++ Presentations/GradSeminar(4-27-22)/command.vim
@@ -0,0 +1,2 @@
+noremap <leader><leader> :w <cr> :!./% &<cr>
+noremap <leader>r :!python3 % & <cr>
--- Presentations/GradSeminar(4-27-22)/randomwalk.py
+++ Presentations/GradSeminar(4-27-22)/randomwalk.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+
 # We generate a random walk with values equalt to the square root of the step size
 # It is a standard result that as the step size goes to 0, that the walk will ...
 # ... converge to a brownian motion.
 
-import numpy as np 
-import matplotlib.pyplot as plt
+IMPORT numpy as np
+IMPORT matplotlib.pyplot as plt
 
-# pick the interval for the random walk 
-a = 0
-b = 5
+# pick the interval for the random walk
+a = 3
+b = 10
 
 # state how many paths you want to plot
 pamt = 2
@@ -17,17 +19,16 @@ d = (b-a)/70
 h = np.sqrt(d)
 
 # create array to store the paths
-x_axis = np.arange(a,b+(d/2),d)
-X = np.zeros((pamt, len(x_axis))) 
+x_axis = np.arange(a, b+(d/2), d)
+X = np.zeros((pamt, len(x_axis)))
 
 for i in range(pamt):
     dy = np.random.choice([-h, h], len(x_axis)-1)
-    y =np.append([0], np.cumsum(dy))
-    X[i, : ] = y
+    y = np.append([0], np.cumsum(dy))
+    X[i, :] = y
 
 for i in range(pamt):
     plt.plot(x_axis, X[i], marker='o')
 
-plt.title('{} sample paths of a random walk \n jump size = {} and step size = {} \n number of steps = {}'.format(pamt, round(h,3), round(d, 2), len(dy)))
+plt.title('{} sample paths of a random walk \n jump size = {} and step size = {} \n number of steps = {}'.format(pamt, round(h, 3), round(d, 2), len(dy)))
 plt.show()
-
