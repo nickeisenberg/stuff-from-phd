b5f197bd0f7b518adf8ac34e78c908a8f37e22ac
chenle02 <chenle02@gmail.com>
2022-12-29 00:33:56 -0500

    Working on the submission

---
+++ SHE_InvariantMeasure/Submission2_JTP/extractFigure.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# By Le Chen
+# chenle02@gmail.com / le.chen@auburn.edu
+#
+
+import re
+import subprocess
+
+# Open the LaTeX file and read its contents into a string
+with open('./Invariant_Measure_SHE.tex', 'r') as f:
+    contents = f.read()
+
+# Find all the TikZ figures in the file using a regular expression
+tikz_figures = re.findall(r'\\begin{tikzpicture}(.+?)\\end{tikzpicture}', contents, flags=re.DOTALL)
+
+# Extract the caption and label for each figure
+captions = []
+labels = []
+for figure in tikz_figures:
+    caption_match = re.search(r'\\caption{(.+?)}', figure)
+    label_match = re.search(r'\\label{(.+?)}', figure)
+    if caption_match:
+        captions.append(caption_match.group(1))
+    else:
+        captions.append('')
+    if label_match:
+        labels.append(label_match.group(1))
+    else:
+        labels.append('')
+
+# Save each figure to a separate file
+for i, figure in enumerate(tikz_figures):
+    with open(f'figure_{i+1}.tikz', 'w') as f:
+        f.write(figure)
+
+# Create a standalone LaTeX file for each TikZ figure
+for i, figure in enumerate(tikz_figures):
+    with open(f'figure_{i+1}.tex', 'w') as f:
+        f.write(f'\\documentclass[varwidth=\\maxdimen]{{standalone}}\n\\usepackage{{tikz}}\n\\begin{{document}}\n\\begin{{tikzpicture}}{figure}\\end{{tikzpicture}}\n\\end{{document}}')
+
+# # Compile each TikZ file into an EPS file
+# for i, figure in enumerate(tikz_figures):
+#     subprocess.run(['pdflatex', f'figure_{i+1}.tex'])
+#     subprocess.run(['pdf2eps', f'figure_{i+1}.pdf'])
+
+# Compile each TikZ file into an EPS file
+for i, (figure, label) in enumerate(zip(tikz_figures, labels)):
+    subprocess.run(['pdflatex', f'-Dfigurelabel={label}', f'figure_{i+1}.tex'])
+    subprocess.run(['pdf2eps', f'figure_{i+1}.pdf'])
+
+# Print the captions and labels for each figure
+for i, (caption, label) in enumerate(zip(captions, labels)):
+    print(f'Figure {i+1}: caption={caption}, label={label}')
